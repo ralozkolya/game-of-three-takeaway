a3ee1e551b889b0c17e1bc2d321e725845d8cf01
Nikoloz Razmadze <raloz1@gmail.com>
2020-10-14 19:37:02 +0400

    Add test environment

--- src/environments/env.ts
+++ src/environments/env.ts
@@ -1,9 +1,16 @@
 import dev from './dev';
+import test from './test';
 import IEnvironment from './env.types';
 
 let env: IEnvironment;
 
 // Update once other environments are added
-env = dev;
+switch (process.env.REACT_APP_ENV) {
+    case 'test':
+        env = test;
+        break;
+    default:
+        env = dev;
+}
 
 export default env;
---
+++ src/environments/test.ts
@@ -0,0 +1,7 @@
+import IEnvironment from "./env.types";
+
+const env: IEnvironment = {
+    URL: 'http://razmadze.me:3000'
+};
+
+export default env;
