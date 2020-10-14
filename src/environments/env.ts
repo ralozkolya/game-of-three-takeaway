3fef3496f1fa7f3b9105e2ffceee70733140738b
Nikoloz Razmadze <raloz1@gmail.com>
2020-10-14 19:43:46 +0400

    Add prod, change port for test

--- src/environments/env.ts
+++ src/environments/env.ts
@@ -1,5 +1,6 @@
 import dev from './dev';
 import test from './test';
+import prod from './prod';
 import IEnvironment from './env.types';
 
 let env: IEnvironment;
@@ -7,7 +8,12 @@ let env: IEnvironment;
 // Update once other environments are added
 switch (process.env.REACT_APP_ENV) {
     case 'test':
+    case 'testing':
         env = test;
+        break;
+    case 'prod':
+    case 'produnction':
+        env = prod;
         break;
     default:
         env = dev;
--- src/environments/prod.ts
+++ src/environments/prod.ts
@@ -1,7 +1,7 @@
 import IEnvironment from "./env.types";
 
 const env: IEnvironment = {
-    URL: 'http://razmadze.me:3030'
+    URL: 'http://takeaway.razmadze.me'
 };
 
 export default env;
